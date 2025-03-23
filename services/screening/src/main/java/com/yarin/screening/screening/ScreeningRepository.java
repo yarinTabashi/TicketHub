b657e872456b85f1f34229e77bc59465396f3dc6
Yarin Tabashi <yarintabashi@gmail.com>
2025-03-23 12:45:46 +0200

    Refactor ScreeningService.java to adapt to the new seatsAvailabilityMap type

--- services/screening/src/main/java/com/yarin/screening/screening/ScreeningRepository.java
+++ services/screening/src/main/java/com/yarin/screening/screening/ScreeningRepository.java
@@ -2,6 +2,8 @@ package com.yarin.screening.screening;
 
 import com.yarin.screening.dtos.ScreeningResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,5 +11,6 @@ import java.util.List;
 public interface ScreeningRepository extends JpaRepository<Screening, Integer> {
     // Return ScreeningResponse and not screening, so that it's not fetch the avaliabilty map, to do it easier method
     List<ScreeningResponse> findByMovieId(Integer movieId);
+    @Query("SELECT s.availableSeats FROM Screening s WHERE s.movieId = :movieId")
+    Integer findAvailableSeatsByMovieId(@Param("movieId") Integer movieId);
 }
-
